9e8d1e84457291e31af24d7aa2182d8bd4eebc88
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-09-27 21:11:04 +0300

    Check REST

--- src/main/java/com/ranking/hachathon/DummyRestController.java
+++ src/main/java/com/ranking/hachathon/DummyRestController.java
@@ -1,12 +1,12 @@
 package com.ranking.hachathon;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping(value = "/api", produces = "application/json")
 public class DummyRestController {
   @Value("${spring.datasource.url}")
   private String dbUrl;
